66d66dac16cb0c9482391048229f3aab23d5cc6d
käblik <kairit.kivima@khk.ee>
2017-12-08 09:27:49 +0200

    kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: käblik
+ * Date: 24.11.2017
+ * Time: 15:45
+ */
+
+# see on python-i yherealine kommentaar, kuid töötab ka php-s
+
+// see on php yherealine kommentaar
+
+/**
+ * see on php mitmerealine kommentaar
+ * veel yks rida
+ * ja veel yks rida
+ * kommentaar ei ole veebile nähtav
+ */
+
+
+
+?>
+Tere tere
