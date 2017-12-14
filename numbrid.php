15bbd9b02354cb3e6765daa1ccb21ceda5d3aeff
käblik <kairit.kivima@khk.ee>
2017-12-14 14:04:06 +0200

    käsitletud ++ ja -- operaatorid

--- numbrid.php
+++ numbrid.php
@@ -5,7 +5,24 @@
  * Date: 14.12.2017
  * Time: 12:19
  */
+for ($arv = 1; $arv <=10; $arv++)
+{
+    if($arv % 2 == 0)
+    {$varv = 'blue';}
+    else {$varv = 'red';}
 
+    echo '<font color="'.$varv.'">'.$arv.'</font><br />';
+}
+//
+
+$a = 5;
+$b = 2;
+$c = ++$a;
+$d = --$b;
+echo 'a = ' . $a . '<br/>';
+echo 'b = ' . $b . '<br/>';
+echo 'c = ' . $c . '<br/>';
+echo 'd = ' . $d . '<br/>';
 
 
 ?>
