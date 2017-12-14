a3da573a1e3777df6ab81fb5902d0a8b4a282737
käblik <kairit.kivima@khk.ee>
2017-12-14 13:52:42 +0200

    dechex juhuslike värvide valmistamine

--- varv.php
+++ varv.php
@@ -9,7 +9,8 @@
 for($rida = 1; $rida <= 5; $rida++) {
     $varv = '#';
     for($kord = 1; $kord <= 6; $kord++) {
-        $symbol = rand(0,9);
+        $juhuarv = rand(0,15);
+        $symbol = dechex($juhuarv);
         $varv = $varv.$symbol;
     }
     echo '<font color="'.$varv.'">'.
