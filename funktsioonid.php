2803327679adf2ebd35bd41ade191d7ea04a6b22
käblik <kairit.kivima@khk.ee>
2017-12-14 15:00:03 +0200

    parandatud funktsiooni muutuja echo ${....

--- funktsioonid.php
+++ funktsioonid.php
@@ -11,13 +11,13 @@ function tabel($sona1, $sona2, $sona3, $sona4) {
         for($rida = 1; $rida <= 4; $rida++){
             echo '<tr>';
                  echo '<td>';
-                      echo ${'sona'} . $rida;
+                      echo ${'sona' . $rida};
                  echo '</td>';
             echo '</tr>';
         }
     echo '</table>';
 }
-tabel('1', '2', '3', '4');
+tabel('Kui rohkem kysimusi', 'pole', 'siis ma voin', 'teid koju lubada');
 
 //
 
