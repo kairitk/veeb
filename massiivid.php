6662254f40f244731d0c16b32c89b0decc5d6406
käblik <kairit.kivima@khk.ee>
2017-12-21 15:34:43 +0200

    ylesanded 1-3 lahendused

--- massiivid.php
+++ massiivid.php
@@ -38,4 +38,89 @@ foreach ($arvud as $arv){
 }
 
 
+
+
+//Ülesanded
+/*
+ * 1.
+ * Koosta funktsioon nimega looMassiiv,
+ * mis võtab parameetrina massiivi elementide
+ * arvu ja loob juhuarvudest (täisarvud)
+ * vahemikus 100-999 koosneva massiivi.
+ * Loodud massiiv tuleb tagastada
+ * põhiprogrammile ja kontrollida sisu
+ * testväljastuse abil - näiteks print_r
+ */
+function looMassiiv($elementideArv){
+    $massiiv = array();
+    for($kord = 1; $kord <= $elementideArv; $kord++){
+        $juhuarv = rand(100, 999);
+        $massiiv[] = $juhuarv;
+    }
+    return $massiiv;
+}
+$testMassiiv = looMassiiv(5);
+echo '<pre>';
+print_r($testMassiiv);
+echo '</pre>';
+echo '<hr />';
+
+/*
+ * 2.
+ * Loo funktsioon nimega valjastaMassiiv,
+ * mis võtab parameetrina massiivi ja
+ * väljastab tema elemendid
+ * üheveerulise tabeli kujul.
+ * Väljundi loomisel kasuta foreach tsüklit.
+ */
+function valjastaMassiiv($massiiv){
+    echo '<table border="1">';
+    foreach ($massiiv as $element){
+        echo '<tr>';
+        echo '<td>';
+        echo $element;
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+valjastaMassiiv($testMassiiv);
+echo '<hr/>';
+
+/*
+ * 3.
+ * Loo funktsioon nimega vahetaMinMax, mis võtab
+ * parameetrina täisarvude massiivi (testimises
+ * võib kasutada looMassiiv funktsiooniga
+ * genereeritud massiivi)
+ * ja leiab kõige väiksema ja kõige suurema
+ * elemendi antud massiivis ning vahetab nende asukohad.
+ * Kontrolli kas kohad on vahetatud kasutades
+ * valjastaMassiiv funktsiooni.
+ */
+function vahetaMinMax($massiiv){
+    $min = min($massiiv);
+    $max = max($massiiv);
+    echo 'Minimum = ' . $min . '<br/>';
+    echo 'Maximum = ' . $max . '<br />';
+    for ($i = 0; $i < count($massiiv); $i++){
+        if($massiiv[$i] == $min){
+            $massiiv[$i] = $max;
+        } else if($massiiv[$i] == $max){
+            $massiiv[$i] = $min;
+        }
+        // echo $massiiv[$i] . '<br/>';
+    }
+}
+vahetaMinMax($testMassiiv);
+valjastaMassiiv($testMassiiv);
+echo '<hr/>';
+
+
+/*
+ * 4.
+ *
+ */
+
+
 ?>
