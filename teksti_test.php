40516efef1c0d089d8d2acc7a9d5ad1485739f57
käblik <kairit.kivima@khk.ee>
2018-01-25 10:56:35 +0200

    Loodud teksti objekt ja kontrollitud tema tööd.

--- teksti_test.php
+++ teksti_test.php
@@ -6,5 +6,17 @@
  * Time: 10:47
  */
 
+require_once 'tekst.php';
+
+// loome teksti objekti
+$minuTekst = new tekst();
+//määrame tekstile konkreetne sisu
+$minuTekst -> maaraTekst('Tere Maailm!');
+// vaatame, mis on objekti sees
+echo '<pre>';
+print_r($minuTekst);
+echo '</pre';
+// prindime välja
+$minuTekst ->prindiTekst();
 
 ?>
